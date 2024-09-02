8a0b16b1574e028f2d7e2949f4f504bc3765c019
borisenkovvlad <borisenkov@inbox.ru>
2024-09-02 09:57:19 +0300

    исправил

--- module_7_2.py/main.py
+++ module_7_2.py/main.py
@@ -2,7 +2,7 @@ def custom_write(file_name, strings):
     strings_positions = {}
 
     with open(file_name, 'w', encoding='utf-8') as file:
-        for i, string in enumerate(strings,1):
+        for i, string in enumerate(strings):
             start_position = file.tell()  
             file.write(string + '\n')
             strings_positions[(i + 1, start_position)] = string
