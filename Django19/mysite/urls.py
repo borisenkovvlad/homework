0e6e19a94af42d1f984e5f578670d0eefb743564
borisenkovvlad <borisenkov@inbox.ru>
2024-12-25 00:48:59 +0300

    dj_19mvt

--- Django19/mysite/urls.py
+++ Django19/mysite/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for MyDjangoProject project.
+URL configuration for Urban project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,14 +14,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.shortcuts import render
+# from django.views.generic import TemplateView
+# from django.contrib import admin
+# from django.urls import path
+# from task2.views import blog
+
+# urlpatterns = [
+    # path('admin/', admin.site.urls),
+    # path('', menu_page),
+    # path('shop/', shop_page),
+    # path('bin/', bin_page),
+    # path('html/', sign_up_by_html),
+    # path('django/', sign_up_by_django)
+    #path('blog', blog),
+#]
 from django.contrib import admin
 from django.urls import path
-from task1.views import home, shop_games, cart, sign_up_by_django
+from task1_.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('platform', home, name='home'),  # Главная страница
-    path('shop/', shop_games, name='shop'),  # Магазин
-    path('cart/', cart, name='cart'),  # Корзина
-    path('sign_dj/', sign_up_by_django, name='sign_up_by_django'),  # Django
+    path('', main_page),
+    path('shop/', shop_page),
+    path('bin/', bin_page),
+    path('game/', collect_of_games),
+    path('django_sign_up/', sign_up_by_django, name='django_sign_up'),
+    path('html_sign_up/', sign_up_by_html, name='html_sign_up'),
 ]
